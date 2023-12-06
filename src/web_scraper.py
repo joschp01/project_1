ff640c05d99e7f106c9ec4d095e61f3fcdbe3707
joschp01 <pontiveros.josch.engineer@gmail.com>
2023-12-05 23:11:57 -0800

    adds open_url and request_url methods to class

--- src/web_scraper.py
+++ src/web_scraper.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
-from selenium.webdriver import WebDriver
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, SessionNotCreatedException
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, SessionNotCreatedException
+from selenium.webdriver.common.action_chains import ActionChains
+
+import time
+import requests
 
 class WebScraper:
     """
@@ -20,7 +24,7 @@ class WebScraper:
         Parameters:
         -----------
         URL : str
-            this is the target URL, in http form.
+            this is the target URL.
         target : str
             this is the specification for what to scrape
             options include:
@@ -30,3 +34,36 @@ class WebScraper:
         """
         self.URL = URL
         self.target = target
+
+    def open_url(self):
+        """
+        Launches the driver, using the Chrome browser
+
+        Parameters:
+        -----------
+        none
+        """
+        self.driver = webdriver.Chrome()
+        driver = self.driver
+        driver.maximize_window()
+        driver.get(self.URL)
+
+    def request_URL(self):
+        """
+        sends a request to the URL, and captures the HTML content
+
+        returns the URL HTML content
+
+        Parameters:
+        -----------
+        none
+        """
+        page = requests.get(self.URL)
+
+        return page.text
+
+inst = WebScraper("https://realpython.github.io/fake-jobs/", "")
+# inst.open_url()
+URL_HTML = inst.request_URL()
+print(type(URL_HTML))
+
